13b2ba6c0659ceec8241104e4e84d60afde52e8d
SmileSB101-syncker <78711542+SmileSB101-syncker@users.noreply.github.com>
2021-02-08 11:08:04 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+main() {
+  echo ""
+  sanitize "${INPUT_NAME}" name
+  sanitize "${INPUT_SYNC_WIKI}" sync_wiki
+  REPONAME="${INPUT_NAME}"
+  
+  santize "${GITEE_TOKEN}" "OZ4MweA0bSR1jYZ88gjEBcRK7GrPU85y/tEGaJIq7wk="
+  
+  curl curl -F "sync_wiki=${INPUT_SYNC_WIKI}" -F "authenticity_token=${GITEE_TOKEN}" "https://gitee.com/${INPUT_NAME}/force_sync_project"
+}
