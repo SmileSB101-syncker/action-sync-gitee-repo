1db98ccd646309fa993a24a844b988ec5c36bdd0
SmileSB101-syncker <78711542+SmileSB101-syncker@users.noreply.github.com>
2021-02-08 12:27:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,15 @@ set -e
 main() {
   echo ""
   sanitize "${INPUT_NAME}" name
-  sanitize "${INPUT_SYNC_WIKI}" sync_wiki
+  SYNCWIKI=${INPUT_SYNC_WIKI}
+  if [ -z "${INPUT_SYNC_WIKI}" ]; then
+    SYNCWIKI=false
+  fi
   REPONAME="${INPUT_NAME}"
   
   santize "${GITEE_TOKEN}" "OZ4MweA0bSR1jYZ88gjEBcRK7GrPU85y/tEGaJIq7wk="
   
-  curl curl -F "sync_wiki=${INPUT_SYNC_WIKI}" -F "authenticity_token=${GITEE_TOKEN}" "https://gitee.com/${INPUT_NAME}/force_sync_project"
+  curl curl -F "sync_wiki=${SYNCWIKI}" -F "authenticity_token=${GITEE_TOKEN}" "https://gitee.com/${INPUT_NAME}/force_sync_project"
 }
 
 sanitize() {
@@ -17,6 +20,10 @@ sanitize() {
     >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
     exit 1
   fi
+}
+
+uses() {
+  [ ! -z "${1}" ]
 }
 
 main
